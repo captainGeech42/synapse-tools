8034aab8b7ad20129f6ab81fed0fbd6d75d25e28
Zander Work <zander@zanderwork.com>
2025-08-13 22:55:00 -0400

    basic download file done

--- README.md
+++ README.md
@@ -5,3 +5,20 @@
 ```
 $ uv tool install -e .
 ```
+
+## Sample Config
+
+By default, the config is loaded from `~/.syn/tools.yaml`. You can override this `$SYN_TOOLS_CONFIG_PATH`.
+
+```yaml
+optic:
+  # base url of the optic server
+  url: https://optic.my.synapse.instance/
+
+  # creds used to directly authenticate to optic
+  username: admin
+  password: hunter2
+
+  # if true, ssl cert verification is disabled
+  disable_ssl_verification: false
+```
---
+++ src/download_file.py
@@ -0,0 +1,52 @@
+"""Download a file(s) from an Optic's backing Axon."""
+
+import argparse
+import logging
+
+import tqdm
+
+import lib.log as log
+import lib.optic as optic
+
+LOG = logging.getLogger("download_file")
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(prog="download_sample", description=__doc__)
+
+    parser.add_argument("hashes", nargs="+", help="SHA256 hashes to download")
+    parser.add_argument("-v", "--verbose", action="store_true", help="enable verbose/debug logging")
+
+    args = parser.parse_args()
+    for h in args.hashes:
+        if len(h) != 64:
+            parser.error(f"{h} isn't a sha256")
+    
+    return args
+
+def process_hash(c: optic.Client, h: str):
+    try:
+        d = c.axon_files_by_sha256(h)
+    except:
+        logging.exception("failed to download %s", h)
+
+    with open(h, "wb") as f:
+        f.write(d)
+    
+    LOG.info("downloaded %s", h)
+
+def main() -> int:
+    args = parse_args()
+    log.init(args.verbose)
+
+    client = optic.Client()
+
+    if len(args.hashes) > 1:
+        for h in tqdm.tqdm(args.hashes):
+            process_hash(client, h)
+    else:
+        process_hash(client, args.hashes[0])
+
+    return 0
+
+if __name__ == "__main__":
+    main()
